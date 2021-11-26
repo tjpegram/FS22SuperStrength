7b1acca5404d02ae4f33baf3b4afcf6b066133f1
Tyler <tjpegram@vt.edu>
2021-11-25 21:31:35 -0500

    Changed keybind and added MP support - Cred Red Dust Gaming

--- SuperStrength.lua
+++ SuperStrength.lua
@@ -3,18 +3,15 @@ SuperStrength = {};
 function SuperStrength:loadMap(name)
 end; 
 
-
-function SuperStrength:mouseEvent(posX, posY, isDown, isUp, button)
-end; 
-
 function SuperStrength:keyEvent(unicode, sym, modifier, isDown)	
 	if g_currentMission.controlPlayer then
 		
-		if bitAND(modifier, Input.MOD_ALT) > 0 then
-			
-			if isDown and sym == Input.KEY_1 then
+		 if bitAND(modifier, Input.MOD_CTRL) > 0 and bitAND(modifier, Input.MOD_ALT) > 0 and Input.isKeyPressed(Input.KEY_8) then
+			if (g_currentMission.missionDynamicInfo.isMultiplayer == true) and (g_currentMission.player.farmId == FarmManager.SPECTATOR_FARM_ID) then
+				print("SuperStrength -- Player is not farm manager");
+			else
+				g_currentMission.player:consoleCommandToggleSuperStrongMode(SuperStrongMode)
 				print("SuperStrength -- Toggle");
-				g_currentMission.player:consoleCommandToggleSuperStrongMode()
 			end;
 
 		end;
@@ -27,6 +24,9 @@ function SuperStrength:update(dt)
 end; 
 
 function SuperStrength:draw(dt)
+end; 
+
+function SuperStrength:mouseEvent(posX, posY, isDown, isUp, button)
 end; 
 
 addModEventListener(SuperStrength);
